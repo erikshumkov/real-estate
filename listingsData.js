15a202d85633ceac11a406cc5521eef8ca6cca51
Erik Shumkov <erikshumkov@gmail.com>
2020-01-15 13:14:50 +0100

    Initial commit

---
+++ listingsData.js
@@ -0,0 +1,134 @@
+const listingsData = [
+  {
+    id: 1,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 2,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 3,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 4,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 5,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 6,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 7,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 8,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 9,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 10,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 11,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  },
+  {
+    id: 12,
+    address: 'Villagatan 27',
+    city: 'Linkoping',
+    state: 'Östergötland',
+    rooms: 3,
+    price: 3000000,
+    sqft: 80,
+    homeType: 'Apartment',
+    image: ''
+  }
+];
